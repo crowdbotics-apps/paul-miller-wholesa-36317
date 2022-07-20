4917a7c92a25425a5c00986f0632510108e205c2
Crowdbotics <team@crowdbotics.com>
2022-07-20 19:05:51 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,12 +1,25 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native"
 
 export const globalOptions = {
   name: "paul_miller_wholesa_36317",
   url: "https://paul_miller_wholesa_36317.botics.co",
   api: "https://paul_miller_wholesa_36317.botics.co/api/v1"
 }
+
 export const modulesOptions = {
   "@modules/app-menu": {
-    "copy": "Routes available!"
+    copy: "Routes available!"
+  },
+  "@modules/login": {
+    LOGO_URL:
+      "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/cb-icon.png",
+    BACKGROUND_URL:
+      "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/halfbg.png",
+    HOME_SCREEN_NAME: "Paul Miller Login",
+    validateEmail: "^[^\\s]+([.-]?\\w+)*@\\w+([.-]?\\w+)*(.\\w{2,3})+$",
+    SignInNavText: "Sign In",
+    SignUpNavText: "Sign Up",
+    SignInButtonText: "Login",
+    SignUpButtonText: "Sign Up"
   }
 }
